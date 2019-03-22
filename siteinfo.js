b62a29209293d2e19cc6946004f37315861b23e1
wuxiangyang <292545811@qq.com>
2019-03-22 21:33:08 +0800

    修改地址

--- siteinfo.js
+++ siteinfo.js
@@ -1,5 +1,4 @@
 module.exports = {
-  name: "萤火小程序商城",
-  siteroot: "https://xcx.jtwenty.club/", // 必填: api地址，结尾要带/
+  name: "萤火小程序商城", siteroot:"https://xcx.jtwenty.club/", // 必填: api地址，结尾要带/
   uniacid: "10001", // 默认即可，勿填
 };
